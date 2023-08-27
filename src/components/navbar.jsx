f1b54765011d32fab07858f3680c14070addfa89
Vetsa Tarun <93399138+Tarunvetsa@users.noreply.github.com>
2023-08-28 01:55:18 +0530

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { ShoppingCart } from "phosphor-react";
 import "./navbar.css";
 
 export const Navbar = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearch = (event) => {
+    // Add your search functionality here using the searchQuery state
+    console.log("Searching for:", searchQuery);
+  };
+
   return (
     <div className="navbar">
+      <div className="searchBar">
+        <input
+          type="text"
+          className="searchInput"
+          placeholder="Search..."
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          onKeyUp={handleSearch}
+        />
+      </div>
       <div className="links">
         <Link to="/"> Shop </Link>
         <Link to="/cart">
           <ShoppingCart size={32} />
         </Link>
       </div>
+
     </div>
   );
 };
